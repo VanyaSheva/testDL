5f5141e2a517e1c73493b9011883398476077272
Vanya Shevchuk <aggressor2000@gmail.com>
2020-06-17 03:19:43 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ const burger = document.querySelector(".burger");
 const presentsList = document.querySelector(".products-list");
 const circle = document.querySelector(".circle");
 const burgerNav = document.querySelector(".burger-navigation");
+const button = document.querySelector(".scroll-button");
+let Count_Num_Of_Dots = 0;
 
-const presentInfoContainer = document.querySelector(
-  ".description-text-container"
-);
+// инициализация слайдера
+
+docSlider.init({
+  speed: 600,
+  startSpeed: null,
+  easing: "ease",
+  pager: false,
+});
+
+// событие появления тучки
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
@@ -15,6 +24,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 burger.addEventListener("click", handleCircleClick);
 presentsList.addEventListener("click", handlePresentsClick);
+button.addEventListener("click", handleButtonClick);
+
+// функция клика на бургер
 
 function handleCircleClick() {
   burger.classList.toggle("active");
@@ -30,6 +42,8 @@ function handleCircleClick() {
   circle.classList.toggle("circle-open");
 }
 
+// функция клика на подарок
+
 function handlePresentsClick(e) {
   if (e.target === e.currentTarget) {
     return;
@@ -41,14 +55,15 @@ function handlePresentsClick(e) {
   });
   e.target.classList.add("products-list_item--active");
   document.querySelector(".products-description").innerHTML = "";
-  let heading = e.target.dataset.name;
-  let imageSrc = e.target.dataset.img;
-  let text = e.target.dataset.text;
-  let html = markup(heading, text, imageSrc);
+  const heading = e.target.dataset.name;
+  const imageSrc = e.target.dataset.img;
+  const text = e.target.dataset.text;
+  const html = markup(heading, text, imageSrc);
   document
     .querySelector(".products-description")
     .insertAdjacentHTML("beforeend", html);
 }
+// создание разметки под блок с подарком
 
 function markup(heading, text, image) {
   return `
@@ -63,23 +78,29 @@ function markup(heading, text, image) {
          
   `;
 }
+// функция скролла по кнопке
+
+function handleButtonClick() {
+  docSlider.nextPage(600, "ease");
+}
+
+// генератор белых точек
 
 function createDot(x, y) {
   const elem = document.createElement("div");
   elem.setAttribute("class", "dot");
   elem.setAttribute("style", "left:" + x + "px;top:" + y + "px;");
+
   document.getElementsByTagName("body")[0].appendChild(elem);
   return elem;
 }
-
-let Count_Num_Of_Dots = 0;
 
 function addDot() {
   if (document.body.offsetWidth < 764) {
     if (Count_Num_Of_Dots < 15) {
       createDot(
-        Math.floor(Math.random() * document.body.offsetWidth),
-        Math.floor(Math.random() * document.body.offsetHeight)
+        Math.floor(Math.random() * document.body.offsetWidth - 45),
+        Math.floor(Math.random() * document.body.offsetHeight - 45)
       );
       Count_Num_Of_Dots++;
     } else {
@@ -109,22 +130,6 @@ function addDot() {
   }
 }
 
-var My_Timer_Var = setInterval(function () {
+const My_Timer_Var = setInterval(function () {
   addDot();
 }, 0.05);
-
-docSlider.init({
-  speed: 600,
-  startSpeed: null,
-  easing: "ease",
-  pager: false,
-});
-
-const button = document.querySelector(".scroll-button");
-
-button.addEventListener("click", handleButtonClick);
-
-function handleButtonClick() {
-  console.log("hi");
-  docSlider.nextPage(600, "ease");
-}
